3ce25beb8f24450b55055b4393ba9e0b0fc99d1c
Leo Chinchilla <chinchillaleo01@gmail.com>
2026-05-28 11:37:23 +1000

    Update create_project.tcl
    Fix outdated reference to v3.3 of zynq ip

--- vivado/auto_create_project/create_project.tcl
+++ vivado/auto_create_project/create_project.tcl
@@ -56,7 +56,7 @@ create_bd_design $bdname
 
 open_bd_design $projpath/$projName.srcs/sources_1/bd/$bdname/$bdname.bd
 
-create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.3 zynq_ultra_ps_e_0
+create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.5 zynq_ultra_ps_e_0
 
 source $projpath/auto_create_project/ps_config.tcl
 set_ps_config zynq_ultra_ps_e_0
